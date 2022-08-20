2b00545a76a4a8f8f0c0c691dc8e737a7e358ff9
WaritsaraK <6310110443@psu.ac.th>
2022-08-20 17:32:01 +0700

    commit part 2

--- src/Cards.js
+++ src/Cards.js
@@ -3,28 +3,29 @@ import Card from './Card'
 
 function Cards(){
     const [items, setItems] = useState([
-        { id: 1, img: '', stat: "" },
-        { id: 1, img: '', stat: "" },
-        { id: 2, img: '', stat: "" },
-        { id: 2, img: '', stat: "" },
-        { id: 3, img: '', stat: "" },
-        { id: 3, img: '', stat: "" },
-        { id: 4, img: '', stat: "" },
-        { id: 4, img: '', stat: "" },
-        { id: 5, img: '', stat: "" },
-        { id: 5, img: '', stat: "" },
-        { id: 6, img: '', stat: "" },
-        { id: 6, img: '', stat: "" },
-        { id: 7, img: '', stat: "" },
-        { id: 7, img: '', stat: "" },
-        { id: 8, img: '', stat: "" },
-        { id: 8, img: '', stat: "" }
-    ])
+        { id: 1, img: '/pics/Bird.png', stat: "" },
+        { id: 1, img: '/pics/Bird.png', stat: "" },
+        { id: 2, img: '/pics/Crab.png', stat: "" },
+        { id: 2, img: '/pics/Crab.png', stat: "" },
+        { id: 3, img: '/pics/Fish.png', stat: "" },
+        { id: 3, img: '/pics/Fish.png', stat: "" },
+        { id: 4, img: '/pics/Seafish.png', stat: "" },
+        { id: 4, img: '/pics/Seafish.png', stat: "" },
+        { id: 5, img: '/pics/Shell.png', stat: "" },
+        { id: 5, img: '/pics/Shell.png', stat: "" },
+        { id: 6, img: '/pics/Sqiud.png', stat: "" },
+        { id: 6, img: '/pics/Sqiud.png', stat: "" },
+        { id: 7, img: '/pics/Starfish.png', stat: "" },
+        { id: 7, img: '/pics/Starfish.png', stat: "" },
+        { id: 8, img: '/pics/Turtle.png', stat: "" },
+        { id: 8, img: '/pics/Turtle.png', stat: "" }
+    ].sort(()=> Math.random()-0.5))
+
 
     return (
         <div className="container">
             { items.map((item, index) => (
-                <Card />
+                <Card key={index} item={item} />
             ))}
         </div>
     )
